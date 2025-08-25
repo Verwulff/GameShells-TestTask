425d01654ce904c2dd4addc8cb9d48fb767d6197
Verwulff <verowulf@gmail.com>
2025-08-26 01:40:37 +0200

    change 3

--- src/CardTest.java
+++ src/CardTest.java
@@ -4,7 +4,7 @@ public class CardTest {
     public static void main(String args[]) throws IOException {
         if (args.length<1)
             System.out.println("Wrong path");
-        System.out.println("ololo 2");
+        System.out.println("ololo 3");
         Identifier identifier = new Identifier(args[0]);
         identifier.start();
     }
